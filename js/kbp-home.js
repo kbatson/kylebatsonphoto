30cfe80d499e8d4bc91e5e48059c50ad2fa35398
kbatson <kbatson@me.com>
2018-09-04 11:14:02 -0500

    Add CSS and images folders

--- js/kbp-home.js
+++ js/kbp-home.js
@@ -1,3 +1,40 @@
 $(document).ready(function(){
-	alert('working');
+	$("header nav>ul>li>a").on("click", function(){
+		if ($(this).siblings('ul').is(":hidden")){
+			$(this).siblings('ul').slideDown("fast");
+		} else {
+			$(this).siblings('ul').slideUp("fast");
+		}
+		return false;
+	});
+
+	var homeImages = [
+		"DSC03063.jpg",
+		"DSC03065.jpg"
+	];
+
+	var baseURL = "images/home/";
+
+	var counter = 0;
+
+	var slideshow = setInterval(function(){
+		switchImages(counter);
+		if(counter == homeImages.length - 1){
+			counter = 0;
+			//clearInterval(slideshow);
+		} else {
+			counter ++;
+		}
+	}, 5000);
+
+	/*Preload images*/
+	for (var i = 0; i < homeImages.length; i++) {
+		$("<img />").attr("src", baseURL + homeImages[i]);
+	}
+
+	function switchImages(imageIndex){
+		backgroundURL = "url(" + baseURL + homeImages[imageIndex] + ")";
+		console.log(backgroundURL);
+		$('html').css('background-image', backgroundURL);
+	}
 });
