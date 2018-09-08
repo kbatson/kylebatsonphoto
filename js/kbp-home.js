8eb3c5c3a05b83d7f1810df8664fddd012a7763e
kbatson <kbatson@me.com>
2018-09-08 10:24:33 -0500

    Added pre-loader for background images, still needs work

--- js/kbp-home.js
+++ js/kbp-home.js
@@ -1,40 +1,68 @@
 $(document).ready(function(){
+	/* Add plus decoration */
+	$('li').each(function(){
+		if($(this).find('ul').length > 0){
+			$(this).addClass('hasChildren');
+		}
+	})
+
+	/* Expand menu */
 	$("header nav>ul>li>a").on("click", function(){
 		if ($(this).siblings('ul').is(":hidden")){
-			$(this).siblings('ul').slideDown("fast");
+			$(this).siblings('ul').slideDown("fast").parent('li').addClass('expanded');
 		} else {
-			$(this).siblings('ul').slideUp("fast");
+			$(this).siblings('ul').slideUp("fast").parent('li').removeClass('expanded');
 		}
 		return false;
 	});
 
-	var homeImages = [
-		"DSC03063.jpg",
-		"DSC03065.jpg"
-	];
+	var homeImagesArray = [];
+	var homeImages = $("#imageLoader img").each(function(){
+		homeImagesArray.push($(this).attr("src"));
+	});
 
-	var baseURL = "images/home/";
 
 	var counter = 0;
+	var imagesLoaded = 0;
+	
+	var totalImages = $("#imageLoader img").length;
+	console.log(totalImages);
 
-	var slideshow = setInterval(function(){
-		switchImages(counter);
-		if(counter == homeImages.length - 1){
-			counter = 0;
-			//clearInterval(slideshow);
+	$("#imageLoader img").each(function(){
+		if(!$(this).prop('loaded')){
+			console.log("Waiting to be loaded!");
+			$(this).on('load', function() {
+				console.log("Loaded!");
+				$(this).prop('loaded', true);
+				console.log("loaded:", $(this).prop('complete'));
+				//console.log($(this).complete);
+				imagesLoaded ++;
+				if(imagesLoaded === totalImages){
+					console.log('all loaded!');
+					slideshow();
+				}
+			});
 		} else {
-			counter ++;
+			console.log('Already loaded!');
+			imagesLoaded ++;
 		}
-	}, 5000);
+	});
 
-	/*Preload images*/
-	for (var i = 0; i < homeImages.length; i++) {
-		$("<img />").attr("src", baseURL + homeImages[i]);
+	var slideshow = function(){
+		setInterval(function(){
+			switchImages(counter);
+			if(counter == homeImagesArray.length - 1){
+				counter = 0;
+				//clearInterval(slideshow);
+			} else {
+				counter ++;
+			}
+		}, 5000);
 	}
 
 	function switchImages(imageIndex){
-		backgroundURL = "url(" + baseURL + homeImages[imageIndex] + ")";
-		console.log(backgroundURL);
-		$('html').css('background-image', backgroundURL);
+		backgroundURL = homeImagesArray[imageIndex];
+		console.log('switch image', backgroundURL);
+		$('html').attr('style', "background-image: url(" + backgroundURL + ")");
 	}
 });
