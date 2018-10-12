033600f0822c8cbf5839b17840e3ce90ae214103
kbatson <kbatson@me.com>
2018-10-12 13:58:00 -0500

    Moved Vegas script to footer to use Jekyll data

--- site/js/kbp-home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$("body").vegas({
-	    slides: [
-	        { src: "/photos/street/DSC05111.jpg" },
-	        { src: "/photos/street/DSC04119.jpg" },
-	        { src: "/photos/street/DSC00602.jpg" }
-	    ]
-	});
-});
